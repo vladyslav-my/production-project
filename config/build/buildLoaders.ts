9883abaf07312e9eb639e842fddce3c7709666c5
Vladislav <vladis13777@gmail.com>
2023-06-14 23:46:29 +0300

    added stylelint, scripts in package.json

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,50 +1,50 @@
-import webpack from "webpack";
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
-import {BuildOptions} from "./types/config";
+import webpack from 'webpack';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import {BuildOptions} from './types/config';
 
 export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
 
-    const svgLoader = {
-        test: /\.svg$/,
-        use: ['@svgr/webpack']
-    }
+	const svgLoader = {
+		test: /\.svg$/,
+		use: ['@svgr/webpack']
+	};
 
-    const fileLoader = {
-        test: /\.(png|jpe?g|gif|woff|woff2)$/i,
-        use: ['file-loader'],
+	const fileLoader = {
+		test: /\.(png|jpe?g|gif|woff|woff2)$/i,
+		use: ['file-loader'],
        
-    }
+	};
 
-    const cssLoader = {
-        test: /\.s[ac]ss$/i,
-        use: [
-            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-            {
-                loader: "css-loader",
-                options: {
-                    modules: {
-                        auto: (resPath: string) => Boolean(resPath.includes('.module.')),
-                        localIdentName: isDev
-                            ? '[path][name]__[local]--[hash:base64:5]'
-                            : '[hash:base64:8]'
-                    },
-                }
-            },
-            "sass-loader",
-        ],
-    }
+	const cssLoader = {
+		test: /\.s[ac]ss$/i,
+		use: [
+			isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+			{
+				loader: 'css-loader',
+				options: {
+					modules: {
+						auto: (resPath: string) => Boolean(resPath.includes('.module.')),
+						localIdentName: isDev
+							? '[path][name]__[local]--[hash:base64:5]'
+							: '[hash:base64:8]'
+					},
+				}
+			},
+			'sass-loader',
+		],
+	};
 
-    // Якщо використовуємо js - потрібен babel-loader
-    const typescriptLoader = {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-    }
+	// Якщо використовуємо js - потрібен babel-loader
+	const typescriptLoader = {
+		test: /\.tsx?$/,
+		use: 'ts-loader',
+		exclude: /node_modules/,
+	};
 
-    return [
-        fileLoader,
-        svgLoader,
-        typescriptLoader,
-        cssLoader,
-    ]
+	return [
+		fileLoader,
+		svgLoader,
+		typescriptLoader,
+		cssLoader,
+	];
 }
--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,24 +1,24 @@
-import HtmlWebpackPlugin from "html-webpack-plugin";
-import webpack from "webpack";
-import {BuildOptions} from "./types/config";
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
-import ReactRefreshWebpachPlugin from "@pmmmwh/react-refresh-webpack-plugin";
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import webpack from 'webpack';
+import {BuildOptions} from './types/config';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import ReactRefreshWebpachPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 
 export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPluginInstance[] {
 
-    return [
-        new HtmlWebpackPlugin({
-            template: paths.html,
-        }),
-        new webpack.ProgressPlugin(),
-        new MiniCssExtractPlugin({
-            filename: 'css/[name].[contenthash:8].css',
-            chunkFilename: 'css/[name].[contenthash:8].css',
-        }),
-        new webpack.DefinePlugin({
-            __IS_DEV__: JSON.stringify(isDev),
-        }),
-        new ReactRefreshWebpachPlugin,
-        new webpack.HotModuleReplacementPlugin(),
-    ]
+	return [
+		new HtmlWebpackPlugin({
+			template: paths.html,
+		}),
+		new webpack.ProgressPlugin(),
+		new MiniCssExtractPlugin({
+			filename: 'css/[name].[contenthash:8].css',
+			chunkFilename: 'css/[name].[contenthash:8].css',
+		}),
+		new webpack.DefinePlugin({
+			__IS_DEV__: JSON.stringify(isDev),
+		}),
+		new ReactRefreshWebpachPlugin,
+		new webpack.HotModuleReplacementPlugin(),
+	];
 }
