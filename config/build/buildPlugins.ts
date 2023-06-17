d41bb72a2390c729cf1cda4992836e557553b310
Vladislav <vladis13777@gmail.com>
2023-06-18 00:03:04 +0300

    added WebpackBundleAnalyzer

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -3,6 +3,7 @@ import webpack from "webpack";
 import {BuildOptions} from "./types/config";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import ReactRefreshWebpachPlugin from "@pmmmwh/react-refresh-webpack-plugin";
+import {BundleAnalyzerPlugin} from "webpack-bundle-analyzer";
 
 export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPluginInstance[] {
 
@@ -19,6 +20,9 @@ export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPlugi
 			__IS_DEV__: JSON.stringify(isDev),
 		}),
 		new ReactRefreshWebpachPlugin,
-		new webpack.HotModuleReplacementPlugin(),
+		new webpack.HotModuleReplacementPlugin({
+			openAnalyzer: false,
+		}),
+		new BundleAnalyzerPlugin()
 	];
 }
