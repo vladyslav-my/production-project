29e4896fb9c7b3aef5f7290679ec37d399a7b883
Vladislav <vladisl13777@gmail.com>
2023-06-22 01:50:23 +0300

    added ci pipeline

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -6,8 +6,7 @@ import ReactRefreshWebpachPlugin from "@pmmmwh/react-refresh-webpack-plugin";
 import {BundleAnalyzerPlugin} from "webpack-bundle-analyzer";
 
 export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPluginInstance[] {
-
-	return [
+	const plugins = [
 		new HtmlWebpackPlugin({
 			template: paths.html,
 		}),
@@ -19,10 +18,18 @@ export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPlugi
 		new webpack.DefinePlugin({
 			__IS_DEV__: JSON.stringify(isDev),
 		}),
-		new ReactRefreshWebpachPlugin,
-		new webpack.HotModuleReplacementPlugin(),
-		new BundleAnalyzerPlugin({
-			openAnalyzer: false,
-		})
 	];
+
+	if (isDev) {
+		plugins.push(
+			new ReactRefreshWebpachPlugin,
+			new webpack.HotModuleReplacementPlugin(),
+			new BundleAnalyzerPlugin({
+				openAnalyzer: false,
+			})
+		);
+	}
+
+
+	return plugins;
 }
