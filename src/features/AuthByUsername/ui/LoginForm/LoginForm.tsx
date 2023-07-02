2dd197a52059003cb1103d5c81c56b249c90f39e
Vladislav <vladisl13777@gmail.com>
2023-07-02 14:26:07 +0300

    created DynamicReduceLoader

--- src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
@@ -1,39 +1,34 @@
-import { FC, useCallback, useEffect } from "react";
 import cls from "./LoginForm.module.scss";
+
 import { classNames } from "shared/lib/classNames/classNames";
-import { useDispatch, useSelector, useStore } from "react-redux";
-import { getLoginFormState } from "../../selectors/getLoginFormState/getLoginFormState";
-import { loginFormActions, loginFormReducer } from "../../slice/loginFormSlice";
-import { loginByUsername } from "../../services/loginByUsername/loginByUsername";
+import { FC, useCallback } from "react";
+import { Reducer } from "@reduxjs/toolkit";
+import { useDispatch, useSelector } from "react-redux";
 import { Button, ThemeButton } from "shared/ui/Button";
 import { Input } from "shared/ui/Input/Input";
-import { Reducer } from "@reduxjs/toolkit";
-import { ReduxStoreWithManager, StateSchemaKey } from "app/providers/StoreProvider/config/StateSchema";
-import { getLoginFormUsername } from "features/AuthByUsername/selectors/getLoginFormUsername/getLoginFormUsername";
-import { getLoginFormPassword } from "features/AuthByUsername/selectors/getLoginFormPassword/getLoginFormPassword";
-import { getLoginFormIsLoading } from "features/AuthByUsername/selectors/getLoginFormIsLoading/getLoginFormIsLoading";
-import { getLoginFormError } from "features/AuthByUsername/selectors/getLoginFormError/getLoginFormError";
-
-const initialReducers: ReducersList = {
-	loginForm: loginFormReducer,
-};
+import { DynamicReduceLoader } from "shared/lib/components/DynamicReduceLoader";
+import { StateSchemaKey } from "app/providers/StoreProvider/config/StateSchema";
+import { loginByUsername } from "../../services/loginByUsername/loginByUsername";
+import { getLoginFormUsername } from "../../selectors/getLoginFormUsername/getLoginFormUsername";
+import { getLoginFormPassword } from "../../selectors/getLoginFormPassword/getLoginFormPassword";
+import { getLoginFormIsLoading } from "../../selectors/getLoginFormIsLoading/getLoginFormIsLoading";
+import { getLoginFormError } from "../../selectors/getLoginFormError/getLoginFormError";
+import { loginFormActions, loginFormReducer } from "../../slice/loginFormSlice";
 
 export type ReducersList = {
 	[name in StateSchemaKey]?: Reducer;
 }
 
-type ReducersListEntry = [StateSchemaKey, Reducer]
-
 export interface LoginFormProps {
 	className?: string
 }
 
-const removeAfterUnmount = true;
+const initialReducers: ReducersList = {
+	loginForm: loginFormReducer,
+};
 
 const LoginForm: FC<LoginFormProps> = ({ className }) => {
 	const dispatch = useDispatch<any>();
-
-	const store = useStore() as ReduxStoreWithManager;
 
 	const username = useSelector(getLoginFormUsername);
 	const password = useSelector(getLoginFormPassword);
@@ -52,33 +47,19 @@ const LoginForm: FC<LoginFormProps> = ({ className }) => {
 		console.log("loginByUsername");
 		dispatch(loginByUsername({username, password}));
 	}, [dispatch, username, password]);
-
-	
-	useEffect(() => {
-		Object.entries(initialReducers).forEach(([name, reducer]: ReducersListEntry) => {
-			store.reducerManager.add(name, reducer);
-			dispatch({ type: `@INIT ${name} reducer` });
-		});
-
-		return () => {
-			if (removeAfterUnmount) {
-				Object.entries(initialReducers).forEach(([name, reducer]: ReducersListEntry) => {
-					store.reducerManager.remove(name);
-					dispatch({ type: `@DESTROY ${name} reducer` });
-				});
-			}
-		};
-	}, []);
 	
 	return (
-		<div className={classNames(cls.LoginForm, {}, [className])}>
-			<p>{error && "wrong username or password"}</p>
-			<Input onChange={setUsernameHandler} value={username} placeholder="username" className={cls.input} type="text" />
-			<Input onChange={setPasswordHandler} value={password} placeholder="password" className={cls.input} type="text" />
-			<Button onClick={onLoginClick} theme={ThemeButton.OUTLINE} className={cls.button} disabled={isLoading}>
-				Log in
-			</Button>
-		</div>
+		<DynamicReduceLoader reducers={initialReducers} removeAfterUnmount>
+			<div className={classNames(cls.LoginForm, {}, [className])}>
+				<p>{error && "wrong username or password"}</p>
+				<Input onChange={setUsernameHandler} value={username} placeholder="username" className={cls.input} type="text" />
+				<Input onChange={setPasswordHandler} value={password} placeholder="password" className={cls.input} type="text" />
+				<Button onClick={onLoginClick} theme={ThemeButton.OUTLINE} className={cls.button} disabled={isLoading}>
+					Log in
+				</Button>
+			</div>
+		</DynamicReduceLoader>
+
 	);
 };
 
---
+++ src/shared/lib/components/DynamicReduceLoader.tsx
@@ -0,0 +1,38 @@
+import { FC, ReactNode, useEffect } from "react";
+import { useDispatch, useStore } from "react-redux";
+import { Reducer } from "@reduxjs/toolkit";
+import { ReduxStoreWithManager, StateSchemaKey } from "app/providers/StoreProvider/config/StateSchema";
+
+
+type ReducersListEntry = [StateSchemaKey, Reducer]
+
+interface DynamicReduceLoaderProps {
+	children: ReactNode,
+	removeAfterUnmount?: boolean
+	reducers: any,
+}
+
+
+export const DynamicReduceLoader: FC<DynamicReduceLoaderProps> = ({ children, reducers, removeAfterUnmount }) => {
+	const dispatch = useDispatch();
+	const store = useStore() as ReduxStoreWithManager;
+
+	useEffect(() => {
+		Object.entries(reducers).forEach(([name, reducer]: ReducersListEntry) => {
+			store.reducerManager.add(name, reducer);
+			dispatch({ type: `@INIT ${name} reducer` });
+		});
+
+		return () => {
+			if (removeAfterUnmount) {
+				Object.entries(reducers).forEach(([name, reducer]: ReducersListEntry) => {
+					store.reducerManager.remove(name);
+					dispatch({ type: `@DESTROY ${name} reducer` });
+				});
+			}
+		};
+	}, []);
+
+	return children;
+};
+
